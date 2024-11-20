6bd68b8a1de4d897c829c018d159549a8380251e
Svetlan-37 <karassvetlana@yandex.ru>
2024-11-20 20:46:23 +0300

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -24,10 +24,10 @@ elapsed_time = end_time1 - start_time1
 print(f'{elapsed_time} - линейный вызов')
 
 # Многопроцессный вызов
-# if __name__ == '__main__':
-#     start_time2 = datetime.now()
-#     with Pool(4) as pool:
-#         result = pool.map(read_info, filenames)
-#     end_time2 = datetime.now()
-#     elapsed_time = end_time2 - start_time2
-#     print(f'{elapsed_time} - многопроцессный вызов')
+if __name__ == '__main__':
+    start_time2 = datetime.now()
+    with Pool(4) as pool:
+        result = pool.map(read_info, filenames)
+    end_time2 = datetime.now()
+    elapsed_time = end_time2 - start_time2
+    print(f'{elapsed_time} - многопроцессный вызов')
